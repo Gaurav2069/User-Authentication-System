f832e8e190abb4ea9fc74153999c1beeaaf8d13d
Gaurav Raj <114589225+Gaurav2069@users.noreply.github.com>
2025-03-09 21:23:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const userRouter = require("./routes/user.routes");
+const dotenv = require("dotenv");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
